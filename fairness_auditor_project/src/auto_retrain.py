777a53979300eeebba480d722e2e90bb33afb4e8
Yashi . <yashi.107606@stu.upes.ac.in>
2026-05-11 01:51:56 +0530

    Delete fairness_auditor_project directory

--- fairness_auditor_project/src/auto_retrain.py
+++
@@ -1,7 +0,0 @@
-from src.group_bias_mitigation import retrain_with_weights
-
-def auto_retrain(df):
-
-    model, X_test, y_test, preds = retrain_with_weights(df)
-
-    return model, X_test, y_test, preds
